5fa192302bfaa411fe95e04a07992ee90fae53c8
MD AMIR SHOAIB <amir.shoaib9988@gmail.com>
2025-05-11 20:59:56 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
 function Footer() {
     try {
         return (
-            <div data-name="footer" className="text-center py-4 mt-6">
-                <p data-name="credit" className="text-red-500">
+            <div
+                data-name="footer"
+                className="fixed bottom-0 left-1/2 transform -translate-x-1/2 text-center py-2 bg-white shadow-md w-full sm:w-auto sm:px-4 z-50"
+            >
+                <p data-name="credit" className="text-red-500 text-sm">
                     Developed by Shoaib
                 </p>
             </div>
