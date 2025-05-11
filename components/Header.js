57188cd485dd2e08c6e46859b1128ee88e3885da
MD AMIR SHOAIB <amir.shoaib9988@gmail.com>
2025-05-11 20:56:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ function Header() {
     try {
         return (
             <div data-name="header" className="text-center py-4 mb-6">
-                <h1 data-name="title" className="text-4xl font-bold text-white">GURU CALCU</h1>
+                <h1 data-name="title" className="text-4xl font-bold text-white">CALCUMATE</h1>
             </div>
         );
     } catch (error) {
